86e4217ea98e2225fd35c18e6c637403404831cf
Doodle Huang <myjhao@hotmail.com>
2023-03-09 22:43:12 +0800

    try auto

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,45 +67,6 @@ export default({
     colorMode: 'light',
     contributors: false,
     lastUpdated: false,
-    sidebar: [
-          '/readme.md',
-      {
-        text: 'DHW 规定',
-        children: [
-          '/dhw-regulations/regulation-of-information.md',
-          '/dhw-regulations/dhw.md',
-          '/dhw-regulations/standards-used-by-dhw.md',
-          '/dhw-regulations/diplomacy.md',
-          '/dhw-regulations/usage-rules.md',
-          '/dhw-regulations/group-chat-management.md',
-        ],
-      },
-      {
-        text: 'INF 文档',
-        children: [
-          '/inf-docs/inf-faqs.md',
-          '/inf-docs/ai-setup.md',
-          '/inf-docs/announced-blacklist-of-dhw.md',
-          //'/inf-docs/updates-to-inf.md',
-          //'/inf-docs/inf-updating.md',
-          '/inf-docs/downloads.md',
-          '/inf-docs/inf-1.16-quick-state.md',
-          '/inf-docs/inf-1.18-quick-state.md',
-        ],
-      },
-      {
-        text: '关于本站',
-        children: [
-          '/about/contact-us.md',
-          '/about/evolution.md',
-        ],
-      },
-      {
-        text: 'IA-SETUP',
-        children: [
-          '/ia-setup/this-is-just-a-joke.md',
-        ],
-      },
-    ],
+    sidebar: 'auto',
   }),
 })
