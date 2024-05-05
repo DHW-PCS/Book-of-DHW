d0a3a4839d5f353847ccbc7aa44b0d928bdd04f6
L_Ac <2634038893@qq.com>
2024-05-05 22:03:58 +0800

    清除algolia相关内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,5 @@
 import { defaultTheme } from '@vuepress/theme-default'
 import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 export default({
   lang: 'zh-CN',
@@ -8,54 +7,6 @@ export default({
   description: 'DHW 官方用于公开本组资讯与规定的网站。',
   head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
   plugins: [
-    /*docsearchPlugin({
-      apiKey: 'd9c5176210e119499f3f205b633a19c2',
-      appId: '77GC4FUYYC',
-      indexName: 'bookvercel',
-      locales: {
-        '/': {
-          placeholder: '搜索文档',
-          translations: {
-            button: {
-              buttonText: '搜索文档',
-              buttonAriaLabel: '搜索文档',
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: '清除查询条件',
-                resetButtonAriaLabel: '清除查询条件',
-                cancelButtonText: '取消',
-                cancelButtonAriaLabel: '取消',
-              },
-              startScreen: {
-                recentSearchesTitle: '搜索历史',
-                noRecentSearchesText: '没有搜索历史',
-                saveRecentSearchButtonTitle: '保存至搜索历史',
-                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                favoriteSearchesTitle: '收藏',
-                removeFavoriteSearchButtonTitle: '从收藏中移除',
-              },
-              errorScreen: {
-                titleText: '无法获取结果',
-                helpText: '你可能需要检查你的网络连接',
-              },
-              footer: {
-                selectText: '选择',
-                navigateText: '切换',
-                closeText: '关闭',
-                searchByText: '搜索提供者',
-              },
-              noResultsScreen: {
-                noResultsText: '无法找到相关结果',
-                suggestedQueryText: '你可以尝试查询',
-                reportMissingResultsText: '你认为该查询应该有结果？',
-                reportMissingResultsLinkText: '点击反馈',
-              },
-            },
-          },
-        },
-      }
-    }),*/
     backToTopPlugin(),
   ],
   theme: defaultTheme({
